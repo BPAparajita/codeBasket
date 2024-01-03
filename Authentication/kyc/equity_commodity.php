c3b64efeb6d61fbc076dc3f8520f2df9e57b2823
Beauty  priya <sbpriyaaparajita@gmail.com>
2024-01-04 00:23:00 +0530

    kyc form testing

---
+++ Authentication/kyc/equity_commodity.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Equity and Commodity</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <div class="container ">
+        <div class="body">
+            <div class="hello"><form action="/action_page.php">
+                    <div class="row">
+                        <div class="col-25">
+                            <label for="fno">Form No</label>
+                        </div>
+                        <div class="col-75">
+                            <input type="text" id="fno" name="fno" placeholder="Enter your form no">
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-25">
+                            <label for="ccode">Client Code</label>
+                        </div>
+                        <div class="col-75">
+                            <input type="text" id="cCode" name="clientCode" placeholder="Your client code..">
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-25">
+                            <label for="dpId">DP ID</label>
+                        </div>
+                        <div class="col-75">
+                            <input type="text" id="dpId" name="clientCode" placeholder="Your client code..">
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-25">
+                            <label for="b_code">Branch Code</label>
+                        </div>
+                        <div class="col-75">
+                            <input type="text" id="b_code" name="branchCode" placeholder="Your branch code..">
+                        </div>
+                    </div>
+                    <div class="row">
+                        <input type="submit" value="Submit">
+                    </div>
+                </form></div>
+
+            <div class="hii"><form action="">
+                    <div class="row">
+                        <div class="col-25">
+                            <label for="b_code">Application Type</label>
+                        </div>
+                        <div class="col-75">
+                            <label for="">New</label>
+                            <input type="checkbox" name="new" id="">
+                            <label for="">Update</label>
+                            <input type="checkbox" name="Update" id="">
+                        </div>
+                    </div>
+
+                </form></div>
+        </div>
+    </div>
+</body>
+</html>
